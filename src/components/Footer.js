c7ec03d401a9ae0368a8c164c93047ceb16abacd
SeanHaugen <sean.haugen560@gmail.com>
2023-03-31 14:22:33 -0500

    copy change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,7 +48,7 @@ function Footer() {
                 <div className='copy-write'>
                     <p>2023 Showdown Displays</p>
                 </div>
-                <p>Created by Sean Haugen. Source code can be found on <a href="https://github.com/SeanHaugen/Comparison_guide">GitHub</a> and hosted on <a href="https://app.netlify.com/sites/sd-comparison-guide/overview">Netlify</a></p>
+                <p>Source code can be found on <a href="https://github.com/SeanHaugen/Comparison_guide">GitHub</a> and hosted on <a href="https://app.netlify.com/sites/sd-comparison-guide/overview">Netlify</a></p>
             </div>
 
 
