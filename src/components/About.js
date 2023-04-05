908748d3deacd569f0b4b77e7f4e24c72586a3d6
SeanHaugen <sean.haugen560@gmail.com>
2023-04-05 09:54:30 -0500

    copy addition

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,8 @@ function About() {
 
     return (
         <>
+         <p className='note'><strong><em>This guide is for internal reference only. Please do not share link with customers.</em></strong></p>
+         <br />
         <h1 className="title">About The Tool</h1>
         <div className="about content">
             <p className="body">Oh boy, this tool sounds like something I'd need an instruction manual just to figure out how to use it! You can make comparisons easier by letting you input one main thing and then two other things to compare it to. And right now, it only works for exact item numbers. </p> <br /> <p className="body body-two"> But don't worry, if you're like me and have trouble finding anything, you can use the "Find Item" dropdown menu or the search bar to locate the name and corresponding item numbers for the product line you're looking for. Ah, technology, making our lives easier by adding more steps to everything we do!</p>
